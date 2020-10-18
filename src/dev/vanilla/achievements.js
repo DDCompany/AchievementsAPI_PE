97c9796211fdae04f6f7d7fe33684bc29fc69f84
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-10-18 15:10:38 +0300

    Updated achievements to new api

--- src/dev/vanilla/achievements.js
+++ src/dev/vanilla/achievements.js
@@ -1,110 +1,119 @@
 AchievementAPI.loadFrom(__dir__ + "achievements/story.json");
 AchievementAPI.loadFrom(__dir__ + "achievements/nether.json");
 
-Callback.addCallback("ItemUse", function (coords, item) {
+Callback.addCallback("ItemUse", function (coords, item, block, isExternal, player) {
     switch (item.id) {
         case VanillaBlockID.crafting_table:
-            AchievementAPI.give("story", "root");
+            AchievementAPI.give(player, "story", "root");
             break;
         case VanillaItemID.bucket:
             if (item.data === 10) { //lava_bucket
-                AchievementAPI.give("story", "lava_bucket");
+                AchievementAPI.give(player, "story", "lava_bucket");
             }
             break;
         case VanillaItemID.ender_eye:
-            AchievementAPI.give("story", "follow_ender_eye");
+            AchievementAPI.give(player, "story", "follow_ender_eye");
             break;
         case VanillaItemID.potion:
-            AchievementAPI.give("nether", "brew_potion");
+            AchievementAPI.give(player, "nether", "brew_potion");
             break;
     }
 });
 
-Callback.addCallback("DestroyBlock", function (coords, block) {
+Callback.addCallback("DestroyBlock", function (coords, block, player) {
     switch (block.id) {
         case VanillaBlockID.stone:
-            AchievementAPI.give("story", "mine_stone");
+            AchievementAPI.give(player, "story", "mine_stone");
             break;
         case VanillaBlockID.obsidian:
-            AchievementAPI.give("story", "form_obsidian");
+            AchievementAPI.give(player, "story", "form_obsidian");
             break;
         case VanillaBlockID.diamond_ore:
-            AchievementAPI.give("story", "mine_diamond");
+            AchievementAPI.give(player, "story", "mine_diamond");
             break;
         case VanillaBlockID.nether_brick:
-            AchievementAPI.give("nether", "find_fortress");
+            AchievementAPI.give(player, "nether", "find_fortress");
             break;
     }
 });
 
-Callback.addCallback("DimensionLoaded", function (dimensionId) {
-    if (dimensionId === DimensionType.NETHER) {
-        AchievementAPI.give("story", "enter_the_nether");
-        AchievementAPI.give("nether", "root");
-    } else if (dimensionId === DimensionType.END) {
-        AchievementAPI.give("story", "enter_the_end");
+Callback.addCallback("PlayerChangedDimension", function (player, dimension) {
+    if (dimension === DimensionType.NETHER) {
+        AchievementAPI.give(player, "story", "enter_the_nether");
+        AchievementAPI.give(player, "nether", "root");
+    } else if (dimension === DimensionType.END) {
+        AchievementAPI.give(player, "story", "enter_the_end");
     }
 });
 
-Callback.addCallback("EntityHurt", function (attacker) {
-    let typeOfAttacker = Entity.getType(attacker);
+Callback.addCallback("EntityHurt", function (attacker, victim) {
+    const typeOfAttacker = Entity.getType(attacker);
+    if (typeOfAttacker !== 1) {
+        return;
+    }
 
-    if (typeOfAttacker === EntityType.FIREBALL) {
-        AchievementAPI.give("nether", "return_to_sender");
+    const typeOfVictim = Entity.getType(victim);
+    if (typeOfVictim === EntityType.FIREBALL) {
+        AchievementAPI.give(attacker, "nether", "return_to_sender");
     }
 });
 
-Callback.addCallback("EntityDeath", function (entity) {
-    let typeOfVictim = Entity.getType(entity);
+Callback.addCallback("EntityDeath", function (entity, attacker) {
+    const typeOfAttacker = Entity.getType(attacker);
+    if (typeOfAttacker !== 1) {
+        return;
+    }
 
+    const typeOfVictim = Entity.getType(entity);
     switch (typeOfVictim) {
         case EntityType.WHITHER_SKELETON:
             if (Player.getDimension() === DimensionType.NETHER)
-                AchievementAPI.give("nether", "kill_wither_skeleton");
+                AchievementAPI.give(attacker, "nether", "kill_wither_skeleton");
             break;
         case EntityType.GHAST:
             if (Player.getDimension() === DimensionType.NORMAL)
-                AchievementAPI.give("nether", "uneasy_alliance");
+                AchievementAPI.give(attacker, "nether", "uneasy_alliance");
             break;
         case EntityType.BLAZE:
-            AchievementAPI.give("nether", "obtain_blaze_rod");
+            AchievementAPI.give(attacker, "nether", "obtain_blaze_rod");
             break;
         case EntityType.ENDER_DRAGON:
-            AchievementAPI.give("story", "kill_dragon");
+            AchievementAPI.give(attacker, "story", "kill_dragon");
             break;
     }
 });
 
 Callback.addCallback("EntityAdded", function (entity) {
-    let type = Entity.getType(entity);
-    if (type === EntityType.WHITHER)
-        AchievementAPI.give("nether", "summon_wither");
-    else if (type === EntityType.ITEM) {
-        let item = Entity.getDroppedItem(entity);
-        if (item.id === VanillaBlockID.dragon_egg)
-            AchievementAPI.give("nether", "summon_wither");
+    if (Entity.getType(entity) === EntityType.WHITHER) {
+        Debug.message("p");
+        const source = BlockSource.getDefaultForActor(entity);
+        const pos = Entity.getPosition(entity);
+        const range = 40;
+        source.fetchEntitiesInAABB(pos.x - range, pos.y - range, pos.z - range, pos.x + range, pos.y + range, pos.z + range, EntityType.PLAYER, false)
+            .forEach(player =>
+                AchievementAPI.give(player, "nether", "summon_wither"));
     }
 });
 
-Callback.addCallback("VanillaWorkbenchCraft", function (result) {
+Callback.addCallback("VanillaWorkbenchCraft", function (result, container, player) {
     switch (result.id) {
         case VanillaItemID.stone_pickaxe:
-            AchievementAPI.give("story", "upgrade_tools");
+            AchievementAPI.give(player, "story", "upgrade_tools");
             break;
         case VanillaItemID.iron_helmet:
         case VanillaItemID.iron_chestplate:
         case VanillaItemID.iron_leggings:
         case VanillaItemID.iron_boots:
-            AchievementAPI.give("story", "obtain_armor");
+            AchievementAPI.give(player, "story", "obtain_armor");
             break;
         case VanillaItemID.diamond_helmet:
         case VanillaItemID.diamond_chestplate:
         case VanillaItemID.diamond_leggings:
         case VanillaItemID.diamond_boots:
-            AchievementAPI.give("story", "shiny_gear");
+            AchievementAPI.give(player, "story", "shiny_gear");
             break;
         case VanillaItemID.iron_pickaxe:
-            AchievementAPI.give("story", "iron_tools");
+            AchievementAPI.give(player, "story", "iron_tools");
             break;
     }
 });
