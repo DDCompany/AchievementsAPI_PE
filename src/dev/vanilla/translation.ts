0ace3ee787fea25f9ce55ee845ac3500b37bc17c
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-10-18 13:15:44 +0300

    Refactor for new api

--- src/dev/vanilla/translation.ts
+++ src/dev/vanilla/translation.ts
@@ -20,69 +20,91 @@ Translation.addTranslation("achievements_api.nothing", {
 });
 
 //Story
-Translation.addTranslation("achievements.story.root.title", {ru: "Minecraft"});
+Translation.addTranslation("achievements.story.root.title", {en: "Minecraft", ru: "Minecraft"});
 Translation.addTranslation("achievements.story.root.description", {
+    en: "The heart and story of the game",
     ru: "Главная история игры"
 });
 
-Translation.addTranslation("achievements.story.mine_stone.title", {ru: "Каменный век"});
+Translation.addTranslation("achievements.story.mine_stone.title", {en: "Stone Age", ru: "Каменный век"});
 Translation.addTranslation("achievements.story.mine_stone.description", {
+    en: "Mine stone with your new pickaxe",
     ru: "Добудьте камень новой киркой"
 });
 
-Translation.addTranslation("achievements.story.upgrade_tools.title", {ru: "Обновка!"});
+Translation.addTranslation("achievements.story.upgrade_tools.title", {en: "Getting an Upgrade", ru: "Обновка!"});
 Translation.addTranslation("achievements.story.upgrade_tools.description", {
+    en: "Construct a better pickaxe",
     ru: "Сделайте кирку попрочней"
 });
 
-Translation.addTranslation("achievements.story.obtain_armor.title", {ru: "Дресс-код"});
+Translation.addTranslation("achievements.story.obtain_armor.title", {en: "Suit Up", ru: "Дресс-код"});
 Translation.addTranslation("achievements.story.obtain_armor.description", {
+    en: "Protect yourself with a piece of iron armor",
     ru: "Защитите себя железной бронёй"
 });
 
-Translation.addTranslation("achievements.story.lava_bucket.title", {ru: "Горячая штучка"});
+Translation.addTranslation("achievements.story.lava_bucket.title", {en: "Hot Stuff", ru: "Горячая штучка"});
 Translation.addTranslation("achievements.story.lava_bucket.description", {
+    en: "Drain a bucket with lava",
     ru: "Вылейте ведро лавы"
 });
 
-Translation.addTranslation("achievements.story.iron_tools.title", {ru: "И кирка без дела ржавеет"});
+Translation.addTranslation("achievements.story.iron_tools.title", {
+    en: "Isn't It Iron Pick",
+    ru: "И кирка без дела ржавеет"
+});
 Translation.addTranslation("achievements.story.iron_tools.description", {
+    en: "Upgrade your pickaxe",
     ru: "Обновите свою кирку"
 });
 
-Translation.addTranslation("achievements.story.form_obsidian.title", {ru: "Две стихии"});
+Translation.addTranslation("achievements.story.form_obsidian.title", {en: "Ice Bucket Challenge", ru: "Две стихии"});
 Translation.addTranslation("achievements.story.form_obsidian.description", {
+    en: "Form and mine a block of Obsidian",
     ru: "Сформируйте и добудьте блок обсидиана"
 });
 
-Translation.addTranslation("achievements.story.enter_the_nether.title", {ru: "Огненные недра"});
+Translation.addTranslation("achievements.story.enter_the_nether.title", {
+    en: "We Need to Go Deeper",
+    ru: "Огненные недра"
+});
 Translation.addTranslation("achievements.story.enter_the_nether.description", {
+    en: "Build, light and enter a Nether Portal",
     ru: "Постройте, зажгите и войдите в портал Нижнего мира"
 });
 
-Translation.addTranslation("achievements.story.follow_ender_eye.title", {ru: "Недреманное око"});
+Translation.addTranslation("achievements.story.follow_ender_eye.title", {en: "Eye Spy", ru: "Недреманное око"});
 Translation.addTranslation("achievements.story.follow_ender_eye.description", {
+    en: "Follow an Ender Eye",
     ru: "Следуйте за оком Края"
 });
 
-Translation.addTranslation("achievements.story.enter_the_end.title", {ru: "Конец?"});
+Translation.addTranslation("achievements.story.enter_the_end.title", {en: "The End?", ru: "Конец?"});
 Translation.addTranslation("achievements.story.enter_the_end.description", {
+    en: "Enter the End Portal",
     ru: "Войдите в портал Края"
 });
 
-Translation.addTranslation("achievements.story.mine_diamond.title", {ru: "Алмазы!"});
+Translation.addTranslation("achievements.story.mine_diamond.title", {en: "Diamonds!", ru: "Алмазы!"});
 Translation.addTranslation("achievements.story.mine_diamond.description", {
+    en: "Acquire diamonds",
     ru: "Добудьте алмазы"
 });
 
-Translation.addTranslation("achievements.story.shiny_gear.title", {ru: "Осыпь меня алмазами"});
+Translation.addTranslation("achievements.story.shiny_gear.title", {
+    en: "Cover Me With Diamonds",
+    ru: "Осыпь меня алмазами"
+});
 Translation.addTranslation("achievements.story.shiny_gear.description", {
+    en: "Diamond armor saves lives",
     ru: "Алмазная броня спасает жизни"
 });
 
 //Nether
-Translation.addTranslation("achievements.nether.root.title", {ru: "Нижний мир"});
+Translation.addTranslation("achievements.nether.root.title", {en: "Nether", ru: "Нижний мир"});
 Translation.addTranslation("achievements.nether.root.description", {
+    en: "Bring summer clothes",
     ru: "Захватите летнюю одежду"
 });
 
@@ -91,42 +113,59 @@ Translation.addTranslation("achievements.nether.fast_travel.description", {
     ru: "Воспользуйтесь Нижним миром, чтобы переместиться на 7 км в обычном мире"
 });
 
-Translation.addTranslation("achievements.nether.find_fortress.title", {ru: "Чертоги страха"});
+Translation.addTranslation("achievements.nether.find_fortress.title", {
+    en: "A Terrible Fortress",
+    ru: "Чертоги страха"
+});
 Translation.addTranslation("achievements.nether.find_fortress.description", {
+    en: "Find the nether fortress and break nether bricks",
     ru: "Найдите адскую крепость и добудте адский кирпичи"
 });
 
-Translation.addTranslation("achievements.nether.return_to_sender.title", {ru: "Вернуть отправителю"});
+Translation.addTranslation("achievements.nether.return_to_sender.title", {
+    en: "Return to Sender",
+    ru: "Вернуть отправителю"
+});
 Translation.addTranslation("achievements.nether.return_to_sender.description", {
+    en: "Destroy a Ghast with a fireball",
     ru: "Убейте гаста его же огненным шаром"
 });
 
-Translation.addTranslation("achievements.nether.obtain_blaze_rod.title", {ru: "В полымя"});
+Translation.addTranslation("achievements.nether.obtain_blaze_rod.title", {en: "Into Fire", ru: "В полымя"});
 Translation.addTranslation("achievements.nether.obtain_blaze_rod.description", {
+    en: "Relieve a Blaze of its rod",
     ru: "Добудьте огненный стержень"
 });
 
-Translation.addTranslation("achievements.nether.kill_wither_skeleton.title", {ru: "Бедный Йорик!"});
+Translation.addTranslation("achievements.nether.kill_wither_skeleton.title", {
+    en: "Spooky Scary Skeleton",
+    ru: "Бедный Йорик!"
+});
 Translation.addTranslation("achievements.nether.kill_wither_skeleton.description", {
+    en: "Kill Wither Skeleton",
     ru: "Убейте скелета-иссушителя"
 });
 
-Translation.addTranslation("achievements.nether.summon_wither.title", {ru: "Чудо-юдо"});
+Translation.addTranslation("achievements.nether.summon_wither.title", {en: "Withering Heights", ru: "Чудо-юдо"});
 Translation.addTranslation("achievements.nether.summon_wither.description", {
+    en: "Summon the Wither",
     ru: "Призовите иссушителя"
 });
 
-Translation.addTranslation("achievements.nether.brew_potion.title", {ru: "Местный зельевар"});
+Translation.addTranslation("achievements.nether.brew_potion.title", {en: "Local Brewery", ru: "Местный зельевар"});
 Translation.addTranslation("achievements.nether.brew_potion.description", {
+    en: "Drink potion",
     ru: "Приготовьте и выпейте зелье"
 });
 
-Translation.addTranslation("achievements.nether.uneasy_alliance.title", {ru: "Война миров"});
+Translation.addTranslation("achievements.nether.uneasy_alliance.title", {en: "Uneasy Alliance", ru: "Война миров"});
 Translation.addTranslation("achievements.nether.uneasy_alliance.description", {
+    en: "Rescue a Ghast from the Nether, bring it safely home to the Overworld... and then kill it",
     ru: "Заманите гаста в обычный мир… а затем убейте его"
 });
 
-Translation.addTranslation("achievements.story.kill_dragon.title", {ru: "Освободите Край"});
+Translation.addTranslation("achievements.story.kill_dragon.title", {en: "Free the End", ru: "Освободите Край"});
 Translation.addTranslation("achievements.story.kill_dragon.description", {
+    en: "Good luck",
     ru: "Удачи!"
 });
